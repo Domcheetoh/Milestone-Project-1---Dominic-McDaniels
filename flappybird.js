a6ff264e46afc77d0d4ae24caf7b8d4e683d0ecc
Dominic <domi209m@gmail.com>
2023-11-04 13:57:54 -0700

    Score done

--- flappybird.js
+++ flappybird.js
@@ -34,6 +34,9 @@ let velocityX = -2; // Remember do negative to make pipes go left //
 let velocityY = 0; // Bird jump speed, on 0 bird is not jumping. Negative means bird goes up, positive means bird goes down i think??
 let gravity = 0.4; 
 
+let gameOver = false;
+let score = 0;
+
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
@@ -65,21 +68,45 @@ window.onload = function() {
 
 function update() {
     requestAnimationFrame(update);
+    if (gameOver) {
+        return;
+    }
     context.clearRect(0, 0, board.width, board.height)
 // Bird Details
     velocityY += gravity;
     bird.y = Math.max(bird.y + velocityY, 0); // Will make sure bird  can't fly over screen //
     context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
 
+    if (bird.y > board.height) {
+        gameOver = true;
+    }
+
     // For Pipes //
     for (let i = 0; i < pipeArray.length; i++) {
         let pipe = pipeArray[i];
         pipe.x += velocityX;
         context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height); 
+
+        if (!pipe.passed && bird.x > pipe.x + pipe.width) // pipe.x is left corner of pipe, pipe.width gets right corner of pipe //
+         {
+            score +=0.5; // I had it as 1 but interval for score goes up by 2 since it's passing 2 pipes.
+            pipe.passed = true;
+        } 
+
+        if(detectCollosion(bird, pipe))
+            gameOver = true;
     }
+
+    // Score stuff //
+    context.fillStyle = "white";
+    context.font="45px sans-serif";
+    context.fillText(score, 5, 45);
 }
 
 function placePipes() {
+    if (gameOver) {
+        return;
+    }
 
     let randomPipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2);  // Should change pipe height, come back here if issue with pipe height //
     let space = board.height/4  // Space for bird to go through is 1/4th of board height, change if space is too big or too narrow
@@ -112,3 +139,9 @@ function birdJump(e) {
     }
 }
  
+function detectCollosion (a, b){
+    return a.x < b.x + b.width &&
+            a.x + a.width > b.x &&
+            a.y < b.y + b.height &&
+            a.y + a.height > b.y;
+}
